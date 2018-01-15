cd4a81fe171340202fbacfebfacb2d759347040b
Karolina Boduch <boduch.karolina@wp.pl>
2018-01-15 12:03:23 +0100

    inicjalizacja

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+
+<?php
+	session_start();
+
+	$username ="santaclaus";
+	$password ="goodchildren";
+
+	if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
+		header("Location: children_list.php");
+	}
+
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		if ($_POST['username'] == $username && $_POST['password'] == $password)
+	{
+			$_SESSION['logged_in'] = true;
+			header("Location: children_list.php");  
+		}
+}
+?>
+
+<html>
+	<body>
+		<form method="post" action="login.php">
+			Username:<br/>
+			<input type="text" name="username">
+			Password<br/>
+			<input type="password" name="password">
+			<input type="submit" value="LOG IN">
+		</form>
+	</body>
+
+</html>
